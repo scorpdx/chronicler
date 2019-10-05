c9b2ac910003494cbf3bc6ecf22f0ab6974b6d99
J. Zebedee <zebedee@code.gripe>
2019-10-05 14:13:50 -0500

    Rearrange file structure and remove hardcoded test path

--- test/ChroniclerTests/ChronicleCollectionTest.cs
+++ test/ChroniclerTests/ChronicleCollectionTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Text.Json;
 using Xunit;
 
@@ -7,12 +8,14 @@ namespace ChroniclerTests
 {
     public class ChronicleCollectionTest
     {
-        private static JsonDocument GetCK2Json() => JsonDocument.Parse(File.ReadAllText(@"C:\Users\scorp\Desktop\ck2kok.json"));
+        private static string CK2JsonPath => Path.Combine("resources", "Ironman_West_Francia_HaR.json");
+
+        private static readonly JsonDocument CK2Json = JsonDocument.Parse(File.ReadAllText(CK2JsonPath));
 
         [Fact]
         public void TestParseJson()
         {
-            var chronicleCollection = Chronicler.ChronicleCollection.ParseJson(GetCK2Json());
+            var chronicleCollection = Chronicler.ChronicleCollection.ParseJson(CK2Json);
             Assert.False(string.IsNullOrEmpty(chronicleCollection.Chronicles[0].Chapters[0].Entries[0].Text));
         }
     }
